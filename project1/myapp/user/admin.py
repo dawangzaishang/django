77675c49296f15ea91df3b289a0d685f772db6f1
dawangzaishang <dawangzaishang@163.com>
2020-05-28 14:38:12 +0800

    this is true commit

--- project1/myapp/user/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from myapp.user.models import User,Address
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Address)
-
-
-
--- project1/utils/mixin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.decorators import login_required
-
-#让视图中的类继续此类
-class LoginRequiredMixin(object):
-    @classmethod
-    def as_view(cls, **kwargs):
-        # 调用父类的as_view
-        view = super(LoginRequiredMixin, cls).as_view(**kwargs)
-        return login_required(view)
